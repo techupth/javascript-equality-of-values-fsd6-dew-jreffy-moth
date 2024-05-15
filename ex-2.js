d869b25e33f144080c1f2379ef5cf0f2bfcf9a61
Jeffiros <krittiwat.c@gmail.com>
2024-05-15 13:13:46 +0700

    feat:fill up answers

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,8 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //answer
+null == undefined; //answer
+10 == "10"; //answer
+[10] == 10; //answer
+[1,2] == "1,2"; //answer
+
