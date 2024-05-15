7a1e85751cb89911a0c059b1effd793f7640ca33
Moth <kampanee.moth@gmail.com>
2024-05-15 15:28:58 +0700

    feat: add ex-1 and ex-2 .js

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,37 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
+Object.is(10, 20); // Answer
 Object.is(0, -0);
 Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
+Object.is("one", "two"); // Answer
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // Answer
 Object.is(null, undefined);
 
-10 === 20;
-10 === "10"
+10 === 20; // Answer
+10 === "10";
 3.14 !== 3.14;
 "TechUp" !== "TechUp";
 NaN === NaN;
-0 !== -0;
+0 !== -0; // Answer
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
 objectA === objectB;
+
+console.log(
+  Object.is(10, 20),
+  Object.is(0, -0),
+  Object.is(NaN, 100),
+  Object.is("one", "two"),
+  Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }),
+  Object.is(null, undefined)
+);
+console.log(
+  10 === 20,
+  10 === "10",
+  3.14 !== 3.14,
+  "TechUp" !== "TechUp",
+  NaN === NaN,
+  0 !== -0
+);
+console.log(objectA === objectB);
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,15 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; // Answer
+null == undefined; // Answer
+10 == "10"; // Answer
+[10] == 10; // Answer
+[1, 2] == "1,2"; // Answer
+
+console.log(
+  0 == false,
+  null == undefined,
+  10 == "10",
+  [10] == 10,
+  [1, 2] == "1,2"
+);
